9fd67c08f21303cd34c91d7c75ca5b5d120d25d0
Abhishek Kumar <abhishekint16@gmail.com>
2018-06-18 00:26:55 +0530

    init DragGestureDetector to detect dragging over the screen

--- app/src/main/java/abhishekint/com/wellthytinder/app/tinderview/TinderCardStack.kt
+++ app/src/main/java/abhishekint/com/wellthytinder/app/tinderview/TinderCardStack.kt
@@ -2,6 +2,7 @@ package abhishekint.com.wellthytinder.app.tinderview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.FrameLayout
@@ -16,6 +17,7 @@ class TinderCardStack : RelativeLayout
     private var mIndex = 0
     private var mNumVisible = 4
     internal var viewCollection = ArrayList<View>()
+    private lateinit var mCardAnimator: TinderCardStackAnimator
 
     constructor(context: Context?) : super(context)
     {
@@ -42,5 +44,29 @@ class TinderCardStack : RelativeLayout
         addView(v)
     }
 
-    private fun setupAnimation(context: Context?) {}
+    private fun setupAnimation(context: Context?) {
+        val cardView = viewCollection[viewCollection.size - 1]
+        mCardAnimator = TinderCardStackAnimator(viewCollection,context) //creating an object of cardAnimator and passing collection of FrameLayout
+        mCardAnimator!!.initLayout() //initialize the cardAnimator using object
+
+        val dd = DragGestureDetector(this.context, object : DragGestureDetector.DragListener
+        {
+            override fun onDragStart(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDragContinue(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDragEnd(e1: MotionEvent?, e2: MotionEvent): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTapUp(): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        }
+    }
 }
