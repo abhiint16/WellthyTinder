dc368e13ef5bfef614039343fc82023f4f0b2996
Abhishek Kumar <abhishekint16@gmail.com>
2018-06-17 19:58:06 +0530

    creating basic activity

---
+++ app/src/main/java/abhishekint/com/wellthytinder/app/MainActivity.kt
@@ -0,0 +1,16 @@
+package abhishekint.com.wellthytinder.app
+
+import abhishekint.com.wellthytinder.R
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by abhishek on 17-06-2018.
+ */
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
