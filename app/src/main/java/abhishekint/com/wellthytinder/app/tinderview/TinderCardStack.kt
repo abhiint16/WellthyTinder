e7c0e53058340c9e4aa9bbfe0bef1719517467f5
Abhishek Kumar <abhishekint16@gmail.com>
2018-06-17 23:45:55 +0530

    init TinderCardStack

---
+++ app/src/main/java/abhishekint/com/wellthytinder/app/tinderview/TinderCardStack.kt
@@ -0,0 +1,15 @@
+package abhishekint.com.wellthytinder.app.tinderview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+/**
+ * Created by abhishek on 17-06-2018.
+ */
+class TinderCardStack : RelativeLayout
+{
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+}
